f6d2f55b6f9f83d7057132bfa61e37b2167d4c99
Sujith Rajan <sujithrajan2009@gmail.com>
2021-04-20 23:27:58 +0400

    End of Section 11

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -6,6 +6,7 @@ import App from "./app/layout/App";
 import reportWebVitals from "./reportWebVitals";
 import "semantic-ui-css/semantic.min.css";
 import "react-calendar/dist/Calendar.css";
+import "react-datepicker/dist/react-datepicker.css"
 import { store, StoreContext } from "./app/stores/store";
 import { Router } from "react-router-dom";
 import {createBrowserHistory} from 'history';
